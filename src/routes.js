2ced4f9d673be2edb6253763e70099c5c34bd9ec
tonhojorge <ajbfo@hotmail.com>
2022-05-17 20:21:20 -0300

    alterações de rotas

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Cadastro from './components/cadastro/Cadastro.vue';
+import Home from './components/home/Home.vue';
+
+export const routes = [
+
+    { path: '', component: Home, titulo: 'Home' },
+    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' }
+
+];
+
+
+/*path: '' em branco, equivale a = localhost:3000/#/*/
