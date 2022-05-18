c2029a002706b031c4e04bb6d3760c8c3bde24c6
tonhojorge <ajbfo@hotmail.com>
2022-05-17 21:50:38 -0300

    teste

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,83 +1,42 @@
+
 <template>
   <div class="corpo">
 
-    <h1 class="centralizado">{{ titulo }}</h1>
+    <nav>
+      <ul>       
+          <li v-for="route in routes" :key="route.path">
+            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
 
-    <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-    
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto in fotosComFiltro" :key="foto">
-        <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
-        </meu-painel>
-
-      </li>
-    </ul>
+    <router-view></router-view>
 
   </div>
 </template>
-
 <script>
-import Painel from './components/shered/painel/Painel.vue';
+
+
+import { routes }  from './routes';
 
 export default {
 
-  components: {
-    'meu-painel' : Painel
-  },
-
-  data () {
+  data() {
 
     return {
 
-      titulo: "Alurapic",
-      fotos: [],
-      filtro: ''
-
+      routes
     }
-  },
 
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-  
-  created() {
-
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 
-  }
+}
 </script>
 
 <style>
-  .titulo {
-    text-align: center;
-  }
-
   .corpo {
     font-family: Helvetica, sans-serif;
-    margin: 0 auto;
     width: 96%;
-  }
-
-  .lista-fotos {
-    list-style: none;
-  }
-
-  .lista-fotos .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .imagem-responsiva {
-    width: 100%;
+    margin: 0 auto;
   }
 </style>
